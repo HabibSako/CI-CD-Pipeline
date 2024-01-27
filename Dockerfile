fd645c4fb6764957eb615cfd2e93107ed2dd7060
Habib Şako <habibsako@outlook.com>
2024-01-27 22:56:18 +0300

    argocd ve diğer güncellemeler

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,12 @@
-# pull official base image
 FROM python:3.11
 
-# set work directory
 WORKDIR /app
 
-# copy all files to work directory
 COPY . /app
 
-# install dependencies
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
-# expose port
 EXPOSE 5003
 
-# command to run on container start
 CMD ["python", "ornek.py"]
--- ornek.py
+++ ornek.py
@@ -4,18 +4,24 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-class PostalInfo(Resource):
-    def get(self):
-        return {
-            'derslerim': [
-                'Bulut Bilişim',
-                'Oyun Geliştirme',
-                'Açık Kaynak Yazılım Geliştirme',
-                'Büyük Veri Teknolojileri'
-            ]
+class Product(Resource):
+    def get(self, category_id=None):
+        all_categories = {
+            'Bulut Bilişim': ['Google Cloud', 'Docker', 'Jenkins', 'Snyk', 'DockerHub', 'ArgoCD'],
+            'Oyun Geliştirme': ['Unity', 'C#', 'Hyper Casual Game'],
+            'Açık Kaynak Yazılım Geliştirme': ['Git ve Github', 'NGINX', 'Flask', 'API'],
+            'Büyük Veri Teknolojileri': ['Hadoop', 'Hive', 'HBASE', 'MONGODb']
         }
 
-api.add_resource(PostalInfo, '/')
+        if category_id is None:
+            return all_categories
+        elif category_id in all_categories:
+            return {category_id: all_categories[category_id]}
+        else:
+            return {'error': 'Kategori bulunamadı.'}
+
+api.add_resource(Product, '/', '/<string:category_id>')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5003)
+
